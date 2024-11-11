3a75ff5be987c03fc9fc81c036fd7f9ad4ac987d
Bilal Javed <49397841+TheBilalJaved@users.noreply.github.com>
2024-11-11 11:57:44 +0500

    changes in new version

--- example/lib/homepage.dart
+++ example/lib/homepage.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
+
 import 'package:ffmpeg_helper/ffmpeg_helper.dart';
-import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:material_dialogs/widgets/buttons/icon_button.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
+import 'package:flutter/material.dart';
 import 'package:material_dialogs/material_dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
